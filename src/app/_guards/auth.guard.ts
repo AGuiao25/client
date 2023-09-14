3e314afb2038957697af39b724d15d4cdf77febd
DBASYSTEM\Angel.Guiao <AngelGuiao>
2023-09-14 14:01:27 +0800

    Routing and Toastr

---
+++ src/app/_guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+
+ 
+export const authGuard:  CanActivateFn = (route, state) => {
+ 
+  const accountService = inject(AccountService);
+  const toastr = inject(ToastrService);
+
+
+    return accountService.currentUser$.pipe(
+      map(user => {
+        if(user) return true;
+        else {
+          toastr.error('You need to log in');
+          return false;
+        }
+      })
+    );
+  }
+  
+ 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,22 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +34,8 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
